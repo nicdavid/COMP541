4a0975ada3e90b996a0a814807e18023d23f546c
Nick Barnette <nicdavid@live.unc.edu>
2017-09-17 19:00:53 -0400

    Finished lab 4.

---
+++ lab4/lab4.sim/sim_1/behav/elaborate.bat
@@ -0,0 +1,9 @@
+@echo off
+set xv_path=E:\\Vivado\\Vivado\\2017.2\\bin
+call %xv_path%/xelab  -wto 59a115f261db49c49a7dbbad9fb7c9b3 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot vgatimer_10x4_test_behav xil_defaultlib.vgatimer_10x4_test xil_defaultlib.glbl -log elaborate.log
+if "%errorlevel%"=="0" goto SUCCESS
+if "%errorlevel%"=="1" goto END
+:END
+exit 1
+:SUCCESS
+exit 0
---
+++ lab4/lab4.srcs/sim_1/new/displaydriver_10x4_test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/17/2017 01:38:00 PM
+// Design Name: 
+// Module Name: displaydriver_10x4_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module displaydriver_10x4_test(
+
+    );
+endmodule
